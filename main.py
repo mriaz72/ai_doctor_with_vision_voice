754932cb48156bf40d4202e50cc2ce02e35d2541
mriaz72 <muhammadriaz8685@gmail.com>
2026-05-02 18:52:43 +0500

    Initial SetUp

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ai-doctor-2-0!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/brain_for_doctor.py
@@ -0,0 +1,32 @@
+# setup api key
+import os
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+#convert image required format
+import base64
+image_path = "images/acne.jpg"
+
+image_file = open(image_path, "rb")
+encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
+
+# setup multimodel llm
+from groq import Groq
+query = "What skin condition does this patient have?"
+model = "meta-llama/llama-4-scout-17b-16e-instruct"
+message = [
+    {
+        "role":"user",
+        "content":[
+            {
+                "type":"text",
+                "text":query
+            },
+            {
+                "type":"image_url",
+                "image_url":{
+                    "url":f"data:image/jpeg;base64,{encoded_image}"
+                }
+            }
+        ]
+    }
+]
