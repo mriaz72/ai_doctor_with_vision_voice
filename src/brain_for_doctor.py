19d92924c11ba476ded05e09543eaa6d65a5e1b9
mriaz72 <muhammadriaz8685@gmail.com>
2026-05-02 19:16:32 +0500

    Setup brain for doctor

--- src/brain_for_doctor.py
+++ src/brain_for_doctor.py
@@ -1,4 +1,6 @@
 # setup api key
+from dotenv import load_dotenv
+load_dotenv()
 import os
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
@@ -11,6 +13,7 @@ encoded_image = base64.b64encode(image_file.read()).decode("utf-8")
 
 # setup multimodel llm
 from groq import Groq
+client = Groq(api_key=GROQ_API_KEY)
 query = "What skin condition does this patient have?"
 model = "meta-llama/llama-4-scout-17b-16e-instruct"
 message = [
@@ -30,3 +33,7 @@ message = [
         ]
     }
 ]
+chat_completion = client.chat.completions.create(
+    model=model, 
+    messages=message)
+print(chat_completion.choices[0].message.content)
